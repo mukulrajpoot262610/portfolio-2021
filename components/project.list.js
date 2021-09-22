1aeb8e5138d1a1058591c9d3b5a500729b56a9e0
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2021-09-22 19:19:38 +0530

    Completed

---
+++ components/project.list.js
@@ -0,0 +1,81 @@
+const LIST_DATA = [
+    {
+        id: 1,
+        name: 'devFriend.io',
+        link: '',
+        github: '',
+        tech: ['nextjs', 'reactjs', 'tailwind', 'firebase', 'express', 'nodejs', 'mongodb'],
+    },
+    {
+        id: 2,
+        name: 'Dev.to Community Clone',
+        link: 'http://blog-website-mr.herokuapp.com/',
+        github: 'https://github.com/mukulrajpoot262610/Blog-website',
+        tech: ['reactjs', 'styled-comp', 'firebase', 'express', 'nodejs', 'mongodb'],
+    },
+    {
+        id: 3,
+        name: 'Portfolio Website',
+        link: 'https://mukulrajpoot.vercel.app/',
+        github: 'https://github.com/mukulrajpoot262610/portfolio-2021',
+        tech: ['nextjs', 'tailwind', 'vercel', 'gsap'],
+    },
+    {
+        id: 11,
+        name: 'Joke Teller',
+        link: 'https://joke-teller-mr.netlify.app',
+        github: 'https://github.com/mukulrajpoot262610/joke-teller',
+        tech: ['html', 'css', 'text-to-speech', 'js'],
+    },
+    {
+        id: 4,
+        name: 'Trackit',
+        link: 'https://mukulrajpoot.vercel.app/',
+        github: 'https://github.com/mukulrajpoot262610/portfolio-2021',
+        tech: ['html', 'css', 'js', 'netlify'],
+    },
+    {
+        id: 5,
+        name: 'TrackerX',
+        link: 'https://mukulrajpoot.vercel.app/',
+        github: 'https://github.com/mukulrajpoot262610/portfolio-2021',
+        tech: ['reactjs', 'bootstrap', 'nodejs', 'mongodb', 'express'],
+    },
+    {
+        id: 6,
+        name: 'TrackerX',
+        link: 'https://mukulrajpoot.vercel.app/',
+        github: 'https://github.com/mukulrajpoot262610/portfolio-2021',
+        tech: ['reactjs', 'bootstrap', 'nodejs', 'mongodb', 'express'],
+    },
+    {
+        id: 7,
+        name: 'Netflix FrontEnd',
+        link: 'https://netflix-mr.netlify.app',
+        github: 'https://github.com/mukulrajpoot262610/netflix-clone',
+        tech: ['reactjs', 'bootstrap', 'redux', 'axios'],
+    },
+    {
+        id: 8,
+        name: 'Redux Todo App',
+        link: 'https://redux-todo-mr.netlify.app',
+        github: 'https://github.com/mukulrajpoot262610/redux-todo-crud',
+        tech: ['reactjs', 'bootstrap', 'redux'],
+    },
+    {
+        id: 9,
+        name: 'Vaccinator',
+        link: 'https://vaccinator-mr.netlify.app',
+        github: '',
+        tech: ['reactjs', 'materialui', 'redux', 'api'],
+    },
+    {
+        id: 10,
+        name: 'Shopping Cart',
+        link: 'https://vegshop-mr.netlify.app',
+        github: 'https://github.com/mukulrajpoot262610/veg-shop',
+        tech: ['reactjs', 'styled-comp', 'redux', 'api'],
+    },
+]
+
+export default LIST_DATA
